fcdd58015308199da577b3ce1a4fa9c04bcf2ac6
projectcarepop <projectcarepop@gmail.com>
2025-06-11 05:27:43 +0800

    Debugging build error

--- carepop-backend/src/lib/supabase/client.ts
+++ carepop-backend/src/lib/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
-import { config } from '@/config/index';
+import config from '@/config/config';
 
-const supabaseUrl = config.supabase.url;
-const supabaseKey = config.supabase.anonKey;
+const supabaseUrl = config.supabaseUrl;
+const supabaseKey = config.supabaseAnonKey;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Supabase URL and Anon Key must be provided.');
