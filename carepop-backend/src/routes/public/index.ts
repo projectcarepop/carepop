720887c414ed9931f534d9e09d26b125a217969b
projectcarepop <projectcarepop@gmail.com>
2025-06-11 12:26:05 +0800

    builing fix

--- carepop-backend/src/routes/public/index.ts
+++ carepop-backend/src/routes/public/index.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express';
 import clinicRoutes from './clinic.routes';
 import serviceRoutes from './service.routes';
-import authRoutes from './authRoutes';
-import userRoutes from './userRoutes';
+import authRoutes from './auth.routes';
+
 
 const publicRouter = Router();
 
 publicRouter.use('/auth', authRoutes);
 publicRouter.use('/clinics', clinicRoutes);
 publicRouter.use('/services', serviceRoutes);
-publicRouter.use('/users', userRoutes);
 
 export default publicRouter; 
