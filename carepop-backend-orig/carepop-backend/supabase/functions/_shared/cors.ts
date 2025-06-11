a719c05d615d1b0f2dad8cce11fa302ed52e0265
projectcarepop <projectcarepop@gmail.com>
2025-06-11 09:58:29 +0800

    Delete carepop-backend-orig/carepop-backend directory

--- carepop-backend-orig/carepop-backend/supabase/functions/_shared/cors.ts
+++
@@ -1,5 +0,0 @@
-// carepop-backend/supabase/functions/_shared/cors.ts
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*', // Or your specific frontend domain
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-}; 
