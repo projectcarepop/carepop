1d1ecefb0294d3fec57b39fba45b7272e16ba1f9
projectcarepop <projectcarepop@gmail.com>
2025-06-21 09:11:17 +0800

    vercel function fix

--- carepop-backend-new/api/index.ts
+++ carepop-backend-new/api/index.ts
@@ -2,8 +2,4 @@ import app from '../src/app'
 import { handle } from 'hono/vercel'
 
 // Edge runtime is recommended; remove if using node.
-export const config = {
-  runtime: 'edge',
-}
-
 export default handle(app) 
