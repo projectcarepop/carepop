9c3d3669617b1eebd2b257fb1a851dfb42d4d9dc
projectcarepop <projectcarepop@gmail.com>
2025-06-22 02:49:03 +0800

    web- fixing auth

--- carepop-web/middleware.ts
+++ carepop-web/middleware.ts
@@ -1,52 +1,27 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createClient } from '@/utils/supabase/middleware'
+import { type NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  })
+  const { supabase, response } = createClient(request)
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return request.cookies.get(name)?.value
-        },
-        set(name: string, value: string, options: CookieOptions) {
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          })
-        },
-        remove(name: string, options: CookieOptions) {
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
-        },
-      },
-    }
-  )
+  // This will refresh session if expired - required for Server Components
+  await supabase.auth.getSession()
 
   const {
     data: { user },
   } = await supabase.auth.getUser()
 
-  const publicRoutes = ['/login', '/register', '/auth/callback', '/forgot-password'];
+  const publicRoutes = ['/login', '/register', '/forgot-password', '/terms-of-service', '/privacy-policy'];
   const isPublicRoute = publicRoutes.some(route => request.nextUrl.pathname.startsWith(route));
-
+  
+  // prevent users from accessing public routes if they are logged in
+  if (user && isPublicRoute) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+  
+  // prevent users from accessing protected routes if they are not logged in
   if (!user && !isPublicRoute) {
     return NextResponse.redirect(new URL('/login', request.url))
-  }
-
-  if (user && isPublicRoute && !request.nextUrl.pathname.startsWith('/auth/callback')) {
-    return NextResponse.redirect(new URL('/dashboard', request.url))
   }
 
   if (user && request.nextUrl.pathname.startsWith('/admin')) {
@@ -68,11 +43,11 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - auth (don't run middleware on /auth/callback)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!auth|_next/static|_next/image|favicon.ico).*)',
   ],
 }
